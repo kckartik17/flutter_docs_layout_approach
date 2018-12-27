88f0966009ac0c064e9ad6a22f81b72f1bb3a1ad
Kartik Chauhan <kckartik17@gmail.com>
2018-12-27 20:00:58 +0530

    flutter_docs_layout_approach
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+    //   statusBarColor: Colors.transparent, //or set color with: Color(0xFF0000FF)
+    // ));
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'The Muskan PG',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Gurugram, Haryana',
+                  style: TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          SizedBox(
+            width: 4.0,
+          ),
+          Text('5')
+        ],
+      ),
+    );
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, 'Call'),
+          buildButtonColumn(Icons.near_me, 'Route'),
+          buildButtonColumn(Icons.share, 'Share')
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: EdgeInsets.all(32.0),
+      child: Text(
+          '''Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.''',
+          softWrap: true),
+    );
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme:
+            ThemeData(primaryColor: Colors.blue, brightness: Brightness.dark),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('India'),
+            ),
+          ),
+          body: ListView(
+            children: <Widget>[
+              Image.asset(
+                'images/lake.jpg',
+                height: 240.0,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              buttonSection,
+              textSection
+            ],
+          ),
+        ));
+  }
+}
